ef5a9a173c8d2aa9dce316aa203c4606da922ed8
subhasishpaul-ai <76464014+subhasishpaul-ai@users.noreply.github.com>
2021-02-08 09:29:54 +0530

    git push -u origin main Uploading to AWS RF model for Titanic

---
+++ titanic/ml_predict.py
@@ -0,0 +1,11 @@
+def prediction_model(pclass,sex,age,sibsp,parch,fare,embarked,title):
+    import pickle
+    x = [[pclass,sex,age,sibsp,parch,fare,embarked,title]]
+    randomforest = pickle.load(open('titanic_model.sav','rb'))
+    prediction = randomforest.predict(x)
+    if prediction == 0:
+        prediction = "Not survived"
+    else:
+        prediction = "Survived"
+    # print(predictions)
+    return prediction
